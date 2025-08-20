b70dbd1a65df0b44c796df5e6558254974df1568
RupeshBhulode <131619196+RupeshBhulode@users.noreply.github.com>
2025-08-20 10:01:00 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000"; // FastAPI backend
+const API_URL = "https://popo-1.onrender.com"; // FastAPI backend
 
 // Fetch and display videos
 async function loadVideos() {
